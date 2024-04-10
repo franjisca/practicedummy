eccb529c057585e56a0851ea2800af91a891882a
JIYEON <publicjy324@gmail.com>
2024-04-10 14:47:15 +0900

    Initial commit

---
+++ src/main/java/com/example/practicedummy/PracticedummyApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicedummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticedummyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticedummyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicedummy/PracticedummyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicedummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticedummyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
